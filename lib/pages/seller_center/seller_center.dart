786b9503f44e45dc54ae726163c6879c05efef70
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-10-09 09:09:28 +0800

    added cupertinosegmentedcontroller

--- lib/pages/seller_center/seller_center.dart
+++ lib/pages/seller_center/seller_center.dart
@@ -21,9 +21,9 @@ class SellerCenter extends StatefulWidget {
 }
 
 class _SellerCenterState extends State<SellerCenter> {
+  String _selectedValue = 'Pending';
   @override
   Widget build(BuildContext context) {
-    OrderState _selectedSegment = OrderState.pending;
     return Scaffold(
       appBar: const SellerCenterAppbar(stallName: 'Food'),
       body: Padding(
@@ -38,24 +38,66 @@ class _SellerCenterState extends State<SellerCenter> {
             Column(
               children: [
                 CupertinoSegmentedControl(
-                  selectedColor: const Color.fromARGB(255, 222, 15, 57),
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  groupValue: _selectedSegment,
-                  children: const {
-                    
+                  children: const <String, Widget>{
+                    'Pending': Text('Pending'),
+                    'Accepted': Text('Accepted'),
+                    'Ready': Text('Ready')
                   },
-                  onValueChanged: (OrderState value) {
+                  onValueChanged: (String value) {
                     setState(() {
-                      _selectedSegment = value;
+                      _selectedValue = value;
                     });
                   },
+                  groupValue: _selectedValue,
                 ),
                 const SizedBox(height: 12.0),
+                _selectPage()
               ],
             )
           ],
         ),
       ),
     );
+  }
+
+  Widget _selectPage() {
+    switch (_selectedValue) {
+      case 'Pending':
+        return Container(
+          color: Colors.grey,
+          alignment: Alignment.center,
+          child: const Text(
+            'Pending',
+            style: TextStyle(fontSize: 14),
+          ),
+        );
+      case 'Accepted':
+        return Container(
+          color: Colors.grey,
+          alignment: Alignment.center,
+          child: const Text(
+            'Accepted',
+            style: TextStyle(fontSize: 14),
+          ),
+        );
+      case 'Ready':
+        return Container(
+          color: Colors.grey,
+          alignment: Alignment.center,
+          child: const Text(
+            'Ready',
+            style: TextStyle(fontSize: 14),
+          ),
+        );
+      default:
+        return Container(
+          color: Colors.grey,
+          alignment: Alignment.center,
+          child: const Text(
+            'How?',
+            style: TextStyle(fontSize: 14),
+          ),
+        );
+    }
   }
 }
