75c5e9918f9eff7acc1e9fdd7a40f4d0c26f2d6a
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-10-11 00:53:21 +0800

    added food card

---
+++ lib/widgets/seller_center_widgets/food_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class FoodCard extends StatefulWidget {
+  const FoodCard({super.key});
+
+  @override
+  State<FoodCard> createState() => _FoodCardState();
+}
+
+class _FoodCardState extends State<FoodCard> {
+  bool _isActive = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: ClipRRect(
+        child: SizedBox(
+          height: 75,
+          width: 75,
+          child: Image.asset('assets/image'),
+        ),
+      ),
+      title: Row(
+        children: [
+          Text('Food'),
+          Switch(
+              value: _isActive,
+              onChanged: (bool value) {
+                setState(() {
+                  _isActive = value;
+                });
+              })
+        ],
+        
+      ),
+      trailing: PopupMenuButton(
+        itemBuilder: (context) => [
+          const PopupMenuItem(
+            child: ListTile(
+              leading: Icon(Icons.edit),
+              title: Text('Edit'),
+            ),
+          ),
+          const PopupMenuItem(
+            child: ListTile(
+              leading: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              title: Text('Delete'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
