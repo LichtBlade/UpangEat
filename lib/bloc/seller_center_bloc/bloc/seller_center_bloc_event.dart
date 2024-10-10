8d1b4c3cf91749db7b2ce11c07b6a3577ec3ef4f
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-10-11 00:29:50 +0800

    removed seller center bloc

--- lib/bloc/seller_center_bloc/bloc/seller_center_bloc_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'seller_center_bloc_bloc.dart';
-
-sealed class SellerCenterBlocEvent extends Equatable {
-  const SellerCenterBlocEvent();
-
-  @override
-  List<Object> get props => [];
-}
