ad63953e9542a24b6a1b0b0fa62130278fd20cf9
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-10-23 21:02:15 +0800

    added image uploading widget

---
+++ lib/widgets/form_widgets/upload_image_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UploadImageCard extends StatefulWidget {
+  const UploadImageCard({super.key});
+
+  @override
+  State<UploadImageCard> createState() => UploadImageCardState();
+}
+
+class UploadImageCardState extends State<UploadImageCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
